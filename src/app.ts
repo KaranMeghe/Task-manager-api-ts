8907efc18c457f5a75b99481dfe61d3019faec90
Karan Meghe <karanmeghe87@gmail.com>
2026-02-17 15:06:23 +0530

    add: updated packages, and set up server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Task Manager App');
+/** @format */
+// # What data is coming?
+// Where is it coming from?
+// What should happen to that data?
+// Where should I store it?
+// What should I send back?
+
+import express from 'express';
+const app = express();
+const portNumber = 3001;
+
+app.get('/', (req, res) => {
+  return res.send('Hello');
+});
+
+app.listen(portNumber, () => {
+  console.log('Server is listning on port:', portNumber);
+});
